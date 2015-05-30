7291ed340858ad81489d5eea46e4b8d12bd669f5
scturtle <scturtle@gmail.com>
2015-05-30 21:54:13 +0800

    better church

--- church.hs
+++ church.hs
@@ -1,32 +1,44 @@
-import Text.Show.Functions
+{-# LANGUAGE RankNTypes #-}
 
-type Church a = (a -> a) -> a -> a
+{- https://karczmarczuk.users.greyc.fr/Essays/church.html
+-- http://en.wikipedia.org/wiki/Church_encoding
+-}
 
-z :: Church a
-z _ x = x
+newtype Church = Ch (forall a. (a -> a) -> a -> a)
 
-s :: Church a -> Church a
-s n f x = f (n f x)
+z :: Church
+z = Ch $ \_ x -> x
 
-church :: Integer -> Church a
+s :: Church -> Church
+s (Ch n) = Ch $ \f x -> f (n f x)
+
+church :: Integer -> Church
 church 0 = z
-church n = s .church $ n-1
+church n = s . church . pred $ n
 
-unchurch :: Church Integer -> Integer
-unchurch n = n (+ 1) 0
+nat :: Church -> Integer
+nat (Ch n) = n succ 0
 
-add :: Church a -> Church a -> Church a
-add m n f = m f . n f
+add :: Church -> Church -> Church
+add (Ch m) (Ch n) = Ch $ \f -> m f . n f
 
-mul :: Church a -> Church a -> Church a
-mul m n = m . n
+mul :: Church -> Church -> Church
+mul (Ch m) (Ch n) = Ch $ m . n
 
-pow :: Church a -> Church (a -> a) -> Church a
-pow m n = n m
+pow :: Church -> Church -> Church
+pow (Ch m) (Ch n) = Ch $ n m
+
+pre :: Church -> Church
+pre (Ch n) = Ch $ \f x -> n (\g h -> h (g f)) (const x) id
+
+sub :: Church -> Church -> Church
+sub m (Ch n) = n pre m
 
 main :: IO ()
 main = do
-  print $ unchurch $ s.s.s.s $ z
-  print $ unchurch $ add (church 2) (church 2)
-  print $ unchurch $ mul (church 2) (church 2)
-  print $ unchurch $ pow (church 2) (church 2)
+  print $ nat $ s.s.s.s $ z
+  print $ nat $ add (church 2) (church 2)
+  print $ nat $ mul (church 2) (church 2)
+  print $ nat $ pow (church 2) (church 2)
+  print $ nat $ pre (church 5)
+  print $ nat $ sub (church 5) (church 1)
